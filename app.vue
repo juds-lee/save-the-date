86bb6bfc621da6afb2dde5517ddd3517ad776a40
Judy Jungmin Lee <judy@rivalry.com>
2023-11-24 10:29:46 -0500

    small change, add layout

--- app.vue
+++ app.vue
@@ -1,58 +1,10 @@
 <template>
-  <div class="flex justify-center items-center h-screen flex-col">
-    <p>{{ name }}</p>
-    <h3>Kindly reply before June 22 2024</h3>
-     <form @submit.prevent="submitForm" class="flex flex-col items-center justify-center">
-      <label class="mb-1">
-        <input type="radio" v-model="rsvpOption" value="yes"> Yes
-      </label>
-      <label class="mb-1">
-        <input type="radio" v-model="rsvpOption" value="no"> No
-      </label>
-      <div class="flex flex-col">
-        <label class="mb-1" v-if="numberOfGuests === 2">
-       How many guests?
-        <input class="border-2" type="text" v-model="guestNumber">
-      </label>
-       <label class="mb-1">
-       Allergies?
-        <input type="text" class="border-2" v-model="allergies">
-      </label>
-      </div>
-
-
-      <button type="submit" :class="rsvpOption !== null ? 'active' : 'disabled'">Submit</button>
-    </form>
-  </div>
+    <NuxtLayout />
 </template>
-<script setup>
-   import { ref } from 'vue';
-   import { db } from './services/firebaseclient';
-   import { collection, doc, setDoc } from "firebase/firestore"; 
-
-  const rsvpOption = ref(null);
-  const numberOfGuests = ref(2);
-  const name = ref("Jim and Bob")
-  const submitForm = async () => {
-    console.log( rsvpOption.value);
-   const rsvpSubmissionRef = doc(collection(db, "rsvps"));
-   await setDoc(rsvpSubmissionRef, {
-      name: name.value,
-      rsvpOption: rsvpOption.value,
-      numberOfGuests: numberOfGuests.value,
-      // allergies: allergies.value
-   });
-    
+<script>
+export default {
+  components: {
+    rsvp: () => import('@/layouts/rsvp.vue'),
+  },
 };
 </script>
-<style>
-.rsvp {
-  list-style-type:none; 
-}
-.active {
-  background-color: green;
-}
-.disabled {
-  background-color: red;
-}
-</style>
