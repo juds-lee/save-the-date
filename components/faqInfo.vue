c259044be097604c1bbe6908ada200c56da31d34
Judy Jungmin Lee <judy@rivalry.com>
2024-10-03 22:12:36 -0400

    more wip?

--- components/faqInfo.vue
+++ components/faqInfo.vue
@@ -1,5 +1,20 @@
 <template>
-    <div class="bg-slate-800 w-[100vh]">
+    <div class="bg-slate-800">
         <h1 class=" text-white">INFO GOES HERE</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem dolor expedita assumenda quaerat eligendi
+            quae, optio qui eum officia fugiat nisi ullam magnam obcaecati deleniti odit exercitationem, consequuntur
+            quia reiciendis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem dolor expedita assumenda quaerat eligendi
+            quae, optio qui eum officia fugiat nisi ullam magnam obcaecati deleniti odit exercitationem, consequuntur
+            quia reiciendis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem dolor expedita assumenda quaerat eligendi
+            quae, optio qui eum officia fugiat nisi ullam magnam obcaecati deleniti odit exercitationem, consequuntur
+            quia reiciendis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem dolor expedita assumenda quaerat eligendi
+            quae, optio qui eum officia fugiat nisi ullam magnam obcaecati deleniti odit exercitationem, consequuntur
+            quia reiciendis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem dolor expedita assumenda quaerat eligendi
+            quae, optio qui eum officia fugiat nisi ullam magnam obcaecati deleniti odit exercitationem, consequuntur
+            quia reiciendis.</p>
     </div>
 </template>
