3836cab64c4c30ae39fd70005dcff65752e02dc2
Judy Jungmin Lee <judy@rivalry.com>
2025-01-11 17:40:04 -0500

    adds font and colours

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,17 +1,17 @@
 <template>
     <nav class="w-full bg-grey py-6 px-10">
         <ul class="w-full flex flex-row justify-between items-center">
-            <NuxtLink to="/" class="text-white">LOGO HERE</NuxtLink>
+            <NuxtLink to="/" class="text-accent font-greatvibes text-[50px]">J&D</NuxtLink>
             <span class="flex flex-row justify-center space-x-6">
-                <NuxtLink class="text-white" to="/" :class="{ 'underline underline-offset-4': route.path === '/' }">
+                <NuxtLink class="text-accent" to="/" :class="{ 'underline underline-offset-4': route.path === '/' }">
                     Schedule
                 </NuxtLink>
-                <NuxtLink class="text-white" to="/faq"
+                <NuxtLink class="text-accent" to="/faq"
                     :class="{ 'underline underline-offset-4': route.path === '/faq' }">
                     FAQ
                 </NuxtLink>
             </span>
-            <NuxtLink class=" text-white px-7 border rounded" to="/rsvp">RSVP</NuxtLink>
+            <NuxtLink class="text-main px-7 border rounded" to="/rsvp">RSVP</NuxtLink>
         </ul>
 
     </nav>
